d7c64e9ac171f17429d01d1f78fd8a3c93815ce0
Taranchuk <nfhfyxer@gmail.com>
2022-03-05 22:36:58 +0200

    removed settings

--- 1.3/Source/ReGrowthCore/ReGrowthMod.cs
+++ 1.3/Source/ReGrowthCore/ReGrowthMod.cs
@@ -12,29 +12,9 @@ namespace ReGrowthCore
 {
     class ReGrowthMod : Mod
     {
-        public static ReGrowthSettings settings;
         public ReGrowthMod(ModContentPack pack) : base(pack)
         {
-            settings = GetSettings<ReGrowthSettings>();
             new Harmony("Helixien.ReGrowthCore").PatchAll();
-        }
-
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            base.DoSettingsWindowContents(inRect);
-            settings.DoSettingsWindowContents(inRect);
-        }
-
-        // Return the name of the mod in the settings tab in game
-        public override string SettingsCategory()
-        {
-            return "ReGrowth Core";
-        }
-
-        public override void WriteSettings()
-        {
-            base.WriteSettings();
-
         }
     }
 }
